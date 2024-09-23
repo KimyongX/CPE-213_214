e9eda44adb470759773700a1568524b78cbe94d5
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-09-24 00:19:39 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,50 +1,47 @@
-# Import Library
-from ssd1306 import SSD1306_I2C
-from machine import Pin,SoftI2C
+#---Import Library---
 from time import sleep
+from ssd1306 import SSD1306_I2C
+from machine import Pin,SoftI2C,PWM
 from network import STA_IF,WLAN
 import logging
 from arduino_iot_cloud import ArduinoCloudClient
+from hcsr04 import HCSR04
+from servo import Servo
 
-# Import Credential
+#---Import Credential---
 from secrets import WIFI_SSID
 from secrets import WIFI_PASSWORD
 from secrets import DEVICE_ID
 from secrets import CLOUD_PASSWORD
+from secrets import LINE_NOTIFY_TOKEN
 
-# Pin Setup
+#---Function---
+def set_angle(device,angle):
+    while angle<0:
+        angle+=180
+    while angle>180:
+        angle -= 180
+    
+    duty = int(500_000 + angle * 2_000_000/180)
+    device.duty_ns(duty)
+
+#---Pin Setup---
+red_led = Pin(4,Pin.OUT)
+green_led = Pin(2,Pin.OUT)
+blue_led = Pin(15,Pin.OUT)
+
+buzzer = Pin(12,Pin.OUT)
+beep = PWM(buzzer,500)
+beep.duty(500)
+sleep(0.1)
+beep.deinit()
+
+btn1_Pin = Pin(13,Pin.IN)
+
+ultrasonic1 = HCSR04(trigger_pin=25, echo_pin=33, echo_timeout_us=20000)
+ultrasonic2 = HCSR04(trigger_pin=27, echo_pin=26, echo_timeout_us=20000)
+
 oled_Pin = SoftI2C(scl=Pin(22),sda=Pin(21))
-
-# Screen Resolution 
 oled_ScreenWidth = 128
 oled_ScreenHeight = 64
-
-# OLED Interface 
 oled = SSD1306_I2C(oled_ScreenWidth,oled_ScreenHeight,oled_Pin)
-
-def wifi_connect():
-    wlan = WLAN(STA_IF)
-    wlan.active(True)
-    wlan.connect(WIFI_SSID,WIFI_PASSWORD)
-    while not wlan.isconnected():
-        logging.info("Trying to connect to WIFI, Note this may take a while")
-        sleep(0.5)
-    logging.info("WIFI Connected")
-    
-def onMessageChange(client,value):
-    print('Message from Arduino Cloud:{}'.format(value))
-    oled.fill(0)
-    oled.text(str(value),0,0)
-    oled.show()
-    
-    if value == "Hello":
-        client["message"] = "Hello from ESP32"
-    
-if __name__ == "__main__":
-    logging.basicConfig(datefmt="%H:%M:%S",format="%(asctime)s.%(msecs)03d %(message)s",level=logging.INFO)
-    
-    wifi_connect()
-    
-    arduino_client = ArduinoCloudClient(device_id = DEVICE_ID,username=DEVICE_ID, password=CLOUD_PASSWORD)
-    arduino_client.register('message',value=None,on_write=onMessageChange,interval=0.250)
-    arduino_client.start()
